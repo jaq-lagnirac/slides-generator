35c29f7f478a8744a6cc2a1c78ac60023d7d38fd
Justin Caringal <jacfriday10th@gmail.com>
2024-06-18 23:15:38 -0500

    Playing around with PIL and cropping images

--- generate_slides.py
+++ generate_slides.py
@@ -8,6 +8,7 @@ import sys
 import logging
 import argparse
 from pptx import Presentation
+from PIL import Image
 
 # logging formatting
 SCRIPT_PATH = os.path.abspath(__file__)
@@ -25,8 +26,11 @@ DESCRIPTION = '''
 EPILOG = '''
 '''
 
+# constants
+ACCEPTABLE_IMG_TYPES = ['.jpg', '.png', '.jpeg']
 
-def check_dir(dir : str) -> None:
+
+def check_dir(dir : str) -> str:
     """checks the existence of the dir
     
     A function to ensure the directory exists before
@@ -36,7 +40,7 @@ def check_dir(dir : str) -> None:
         dir (str): a relative path to the input dir
     
     Returns:
-        None (exits program early if dir does not exist)
+        str: Returns name of directory
 
     """
 
@@ -44,20 +48,85 @@ def check_dir(dir : str) -> None:
         error(f'Directory {dir} not found.')
         sys.exit(1)
 
-    return
+    if not os.path.isdir(dir):
+        error(f'Input {dir} is not a directory.')
+        sys.exit(1)
+    
+    dirname = os.path.split(os.path.dirname(dir))[-1] # takes last value
+    print(dirname)
+
+    return dirname
+
+
+def is_image(file : str) -> bool | str:
+    """checks if file is an acceptable image format
+    
+    A function to check the validity of the image
+
+    Args:
+        file (str) : the basename of the file
+
+    Returns:
+        bool: Returns True if filetype is acceptable, false otherwise
+
+    """
+
+    _, ext = os.path.splitext(file)
+
+    if ext.lower() not in ACCEPTABLE_IMG_TYPES:
+        error(f'{file} is not an acceptable image type.')
+        return False
+    
+    return True
 
 
 def main() -> None:
     """MAIN FUNCTION"""
 
-    # checks existence of dir
-    check_dir(args.dir)
+    # checks existence of dir, extracts dirname
+    dirname = check_dir(args.dir)
+
+    print(args.dir)
+    print(os.path.dirname(args.dir))
     # inits presentation
     prez = Presentation()
+    
+    # adds title slide
+    slide = prez.slides.add_slide(prez.slide_layouts[0]) # pos=0, Title Slide
+    title = slide.shapes.title
+    subtitle = slide.placeholders[1]
 
-    for x in os.listdir(args.dir):
-        img_path = os.path.join(args.dir, x)
+    # formats title slide
+    title.text = dirname
+    subtitle.text = 'Created using tools developed by Justin Caringal'
 
+    # iterates through image directory to create slides
+    for img_basename in os.listdir(args.dir):
+        # creates relative path to image
+        img_path = os.path.join(args.dir, img_basename)
+
+        # creates new slide
+        slide = prez.slides.add_slide(prez.slide_layouts[8]) # pos=8, Picture with Caption
+        
+        # creates placeholders using idx key, not position
+        title_placeholder = slide.placeholders[0] # pos=1 Title
+        img_placeholder = slide.placeholders[1]  # pos=2 Picture Placeholder
+        caption_placeholder = slide.placeholders[2] # pos=3 Text Placeholder
+
+        title_placeholder.text = img_basename
+        picture = img_placeholder.insert_picture(img_path)
+        pillow_img = Image.open(img_path)
+        print(pillow_img.width, pillow_img.height, type(pillow_img.width))
+        # picture.width = pillow_img.width
+        # picture.height = pillow_img.height
+        picture.crop_top = 0.0
+        picture.crop_bottom = 0.0
+        # picture.crop_left = 0.0
+        # picture.crop_right = 0.0
+        caption_placeholder.text = f'The image from {img_path}'
+
+
+    prez.save(f'{dirname}.pptx')
 
     # for i, x in enumerate(prez.slide_layouts):
     #     print(i, x.name)
@@ -74,33 +143,18 @@ def main() -> None:
     # 9 Title and Vertical Text
     # 10 Vertical Title and Text
         
-    slide = prez.slides.add_slide(prez.slide_layouts[8])
-    for x in slide.placeholders:
-        print(x.name)
-    
-    print('-----')
-
-    # for x in os.listdir(args.dir):
-    #     print(os.path.join(args.dir, x))
+    # slide = prez.slides.add_slide(prez.slide_layouts[8])
+    # for x in slide.placeholders:
+    #     print(x.name)
     #
     # Title 1
     # Picture Placeholder 2
     # Text Placeholder 3
+    
+    # for x in os.listdir(args.dir):
+    #     print(os.path.join(args.dir, x))
 
     sys.exit()
-    
-    placeholder = slide.placeholders[1]  # idx key, not position
-
-    # picture = placeholder.insert_picture('my-image.png')
-    # title_slide_layout = prez.slide_layouts[0]
-    # slide = prez.slides.add_slide(title_slide_layout)
-    # title = slide.shapes.title
-    # subtitle = slide.placeholders[1]
-
-    # title.text = 'Hello, World!'
-    # subtitle.text = 'python-pptx was here!'
-
-    prez.save('test.pptx')
 
 
 if __name__ == '__main__':
