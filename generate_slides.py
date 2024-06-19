994a180573d1112d9cca5e73f28228da09c602c2
Justin Caringal <jacfriday10th@gmail.com>
2024-06-19 01:34:33 -0500

    Added logging statements

--- generate_slides.py
+++ generate_slides.py
@@ -53,12 +53,16 @@ def check_dir(dir : str) -> str:
         error(f'Directory {dir} not found.')
         sys.exit(1)
 
+    info(f'{args.dir} is a valid directory')
+
     # adds safety slash '/' to end of dir str in order to prevent unaccounted outputs
     if dir[-1] != '/': # if last char is not a '/', add a /
         dir += '/'
+        debug('Added safety slash')
     
     dirname = os.path.split(os.path.dirname(dir))[-1] # takes last value
 
+    debug(f'dirname: {dirname}')
     return dirname
 
 
@@ -75,36 +79,32 @@ def is_image(file : str) -> bool | str:
 
     """
 
-    _, ext = os.path.splitext(file)
+    root, ext = os.path.splitext(file)
 
     if ext.lower() not in ACCEPTABLE_IMG_TYPES:
-        error(f'{file} is not an acceptable image type.')
+        warning(f'{file} is not an acceptable image type.')
+        debug(f'Root: {root} - Ext: {ext}')
         return False
     
     return True
-
-
-# def add_textbox(slide : Presentation,
-#                 left : Inches,
-#                 top : Inches,
-#                 width : Inches,
-#                 height : Inches,
-#                 text : str) -> :
 
 
 def main() -> None:
     """MAIN FUNCTION"""
 
     # checks existence of dir, extracts dirname
+    info(f'Checking input directory {args.dir}')
     dirname = check_dir(args.dir)
 
     # inits presentation
+    info('Initializing presentation')
     prez = Presentation()
     # sets aspect ratio to standard Google Slides size
     prez.slide_width = Inches(10)
     prez.slide_height = Inches(5.625)
     
     # adds title slide
+    info('Creating title slide')
     slide = prez.slides.add_slide(prez.slide_layouts[6]) # pos=6, Blank
     # generates title
     left = Inches(1)
@@ -115,6 +115,7 @@ def main() -> None:
     tf = title.text_frame
     p = tf.add_paragraph()
     p.text = dirname
+    debug(f'Title slide text: {dirname}')
     p.font.bold = True
     p.font.size = Pt(32)
     p.alignment = PP_ALIGN.CENTER
@@ -123,7 +124,9 @@ def main() -> None:
     title = slide.shapes.add_textbox(left, top, width, height)
     tf = title.text_frame
     p = tf.add_paragraph()
-    p.text = 'Created using tools developed by Justin Caringal'
+    subtitle_text = 'Created using tools developed by Justin Caringal'
+    p.text = subtitle_text
+    debug(f'Subtitle text: {subtitle_text}')
     p.font.size = Pt(24)
     p.alignment = PP_ALIGN.CENTER
 
@@ -135,6 +138,15 @@ def main() -> None:
     img_top = Inches(1.1)
     img_height = Inches(4.275)
     for img_basename in os.listdir(args.dir):
+        
+        info(f'Creating slide for {img_basename}')
+
+        # creates relative path to image
+        img_path = os.path.join(args.dir, img_basename)
+        
+        # checks validity of image, skips if not valid
+        if not is_image(img_path):
+            continue
 
         # creates new slide
         slide = prez.slides.add_slide(prez.slide_layouts[6]) # pos=6, Blank
@@ -150,11 +162,12 @@ def main() -> None:
         p.font.size = Pt(32)
 
         # adds image to slide
-        img_path = os.path.join(args.dir, img_basename) # creates relative path to image
         image = slide.shapes.add_picture(img_path, left, img_top, height=img_height)
+        info(f'Finished creating slide for {img_basename}')
 
-
-    prez.save(f'{dirname}.pptx')
+    pptx_name = f'{dirname}.pptx'
+    prez.save(pptx_name)
+    info(f'Slides saved to {os.path.abspath(pptx_name)}')
 
     # for i, x in enumerate(prez.slide_layouts):
     #     print(i, x.name)
@@ -174,21 +187,12 @@ def main() -> None:
     # slide = prez.slides.add_slide(prez.slide_layouts[8])
     # for x in slide.placeholders:
     #     print(x.name)
-    #
-    # Title 1
-    # Picture Placeholder 2
-    # Text Placeholder 3
-    
-    # for x in os.listdir(args.dir):
-    #     print(os.path.join(args.dir, x))
-
-    sys.exit()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=DESCRIPTION, epilog=EPILOG)
     parser.add_argument('dir',
-                        help='relpath to directory of images')
+                        help='relative path to directory of images')
     parser.add_argument('-v', '--verbose',
                     action='store_true',
                     help='Set logging level to DEBUG')
